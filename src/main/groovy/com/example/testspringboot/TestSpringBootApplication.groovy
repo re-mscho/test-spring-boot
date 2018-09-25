2c64b6fe72c4ce90e538dcfc701761043fbb30b0
Martin Schoegler <martin.schoegler@robotic-eyes.com>
2018-09-25 10:15:28 +0200

    initial commit

---
+++ src/main/groovy/com/example/testspringboot/TestSpringBootApplication.groovy
@@ -0,0 +1,12 @@
+package com.example.testspringboot
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class TestSpringBootApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run TestSpringBootApplication, args
+	}
+}
